b34acc968f42b9f8c8b53cc759548f73f5056eab
Kaan Caliskan <mail@kcaliskan.com>
2019-11-17 01:14:30 -0500

    google oauth in progress 2

--- routes/services/passport.js
+++ routes/services/passport.js
@@ -2,9 +2,19 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const config = require("config");
 
-const User = import("../../models/User");
+const User = require("../../models/User");
 
-// We create a new instance of Google Strategy.
+passport.serializeUser((user, done) => {
+  //User id is the id of the user's in the "user" schema in mongodb, not Google id or etc
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  let user = await User.findById(id);
+  done(null, user);
+});
+
+// We create a new instance of Google Strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -12,8 +22,14 @@ passport.use(
       clientSecret: config.get("googleClientSecret"),
       callbackURL: "/api/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const newUser = await new User({ googleId: profile.id }).save();
+        done(null, newUser);
+      }
     }
   )
 );
