c96dc2126d71c8b976c2b77cbe23d44dc5cb8c4e
Kaan Caliskan <mail@kcaliskan.com>
2019-12-06 23:25:24 -0500

    navbar css jitter fix

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,46 +9,43 @@ const Landing = ({ auth }) => {
     localStorage.removeItem("token");
   };
 
+  // const authLinks = (
+  //   <Fragment>
+  //     <div>
+  //       <a href="/api/auth/logout" onClick={() => logOutHandler()}>
+  //         Logout
+  //       </a>
+  //     </div>
+  //     <div>
+  //       <Link
+  //         to={location =>
+  //           auth.user === null ? "#" : `/${auth.user.userName}/codewarsresult`
+  //         }
+  //       >
+  //         Codewars
+  //       </Link>
+  //     </div>
+  //     <div>
+  //       <Link
+  //         to={location =>
+  //           auth.user === null ? "#" : `/${auth.user.userName}/settings`
+  //         }
+  //       >
+  //         Settings
+  //       </Link>
+  //     </div>
+  //   </Fragment>
+  // );
+
   const authLinks = (
     <Fragment>
-      <div>
-        <a href="/api/auth/logout" onClick={() => logOutHandler()}>
-          Logout
-        </a>
-      </div>
-      <div>
-        <Link
-          to={location =>
-            auth.user === null ? "#" : `/${auth.user.userName}/codewarsresult`
-          }
-        >
-          Codewars
-        </Link>
-      </div>
-      <div>
-        <Link
-          to={location =>
-            auth.user === null ? "#" : `/${auth.user.userName}/settings`
-          }
-        >
-          Settings
-        </Link>
-      </div>
+      <Navbar />
     </Fragment>
   );
 
   const guestLinks = (
     <Fragment>
       <Navbar />
-      <Link to="/login">
-        <div>hello</div>
-      </Link>
-      <div>
-        <Link to="/register">Register</Link>
-      </div>
-      <div>
-        <Link to="/signin">Login</Link>
-      </div>
     </Fragment>
   );
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,29 +1,52 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const Navbar = () => {
-  return (
-    <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <Link className="navbar-logo" to="/" />
-        <Link className="navbar-logo-text" to="/">
-          AlgoTrack
-        </Link>
-        <div className="navbar-gap" />
-        <Link className="navbar-create-acount" to="/register">
-          <Link to="/register" className="navbar-create-acount-text">
-            <p>GET YOUR ANALYSIS</p> <p>It's free</p>
+const Navbar = ({ auth }) => {
+  const userLinks = <Fragment>userLinks</Fragment>;
+  const guestLinks = (
+    <div className="navbar-full">
+      <div className="navbar-container">
+        <div className="navbar-logo-fr">
+          <Link className="navbar-logo"></Link>
+        </div>
+        <div className="navbar-logo-text-fr">
+          <Link href="/" className="navbar-logo-text">
+            AlgoTrack
           </Link>
-        </Link>
-        <div className="navbar-login">
-          <Link class="navbar-login-button" to="/login">
+        </div>
+        <div className="navbar-gap-fr"></div>
+        <div className="navbar-create-account-fr">
+          <a href="/" className="navbar-create-account-button">
+            <p>GET YOUR ANALYSIS</p>
+            <p>It's free</p>
+          </a>
+        </div>
+        <div className="navbar-login-fr">
+          <a href="/" className="navbar-login-button">
             Log In
-          </Link>
+          </a>
         </div>
       </div>
     </div>
   );
+  return (
+    <Fragment>
+      {!auth.loading && (
+        // <Fragment>{auth.isAuthenticated ? userLinks : guestLinks}</Fragment>
+        <Fragment>{auth.isAuthenticated ? userLinks : guestLinks}</Fragment>
+      )}
+    </Fragment>
+  );
 };
 
-export default connect(null)(Navbar);
+Navbar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Navbar);
