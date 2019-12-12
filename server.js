4790682f86daa950c021aa77a748fddce3c21123
Kaan Caliskan <mail@kcaliskan.com>
2019-12-12 00:09:51 -0500

    prepared for deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const passport = require("passport");
+const path = require("path");
+
 const app = express();
 
 // Connect Database
@@ -27,6 +29,16 @@ app.use("/api/auth", require("./routes/api/auth"));
 
 //Auth routes handler
 app.use("/api/user", require("./routes/api/user"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
