89213ffee28de7e0264bb4e08f6e97fb8e18d314
Kaan Caliskan <mail@kcaliskan.com>
2019-12-12 01:06:38 -0500

    removed favicon

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
