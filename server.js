52f17170d667ef33391e5f14e48204711cf5cf4f
Kaan Caliskan <mail@kcaliskan.com>
2019-12-12 01:13:27 -0500

    app listen mistake fix

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+// app.listen(PORT);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
