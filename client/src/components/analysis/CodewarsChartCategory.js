4b96d3dc36d52bbe3a54aab3453cadf218a4f15e
Kaan Caliskan <mail@kcaliskan.com>
2019-12-14 15:54:57 -0500

    charts NaN issue fix

--- client/src/components/analysis/CodewarsChartCategory.js
+++ client/src/components/analysis/CodewarsChartCategory.js
@@ -1,40 +1,50 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Chart from "react-apexcharts";
 import { connect } from "react-redux";
 
 class CodewarsChartCategory extends React.Component {
   state = {
-    options: {
-      plotOptions: {
-        bar: {
-          horizontal: true,
-          columnWidth: "10%"
-        }
-      },
-      dataLabels: {
-        enabled: true
-      },
-      xaxis: {
-        categories: this.props.codewarsProfile.completedByCategory
-          ? this.props.codewarsProfile.completedByCategory
-              .completedChallangesByCatName
-          : []
-      },
-      title: {
-        text: "Completed Challanges By Category",
-        align: "center"
-      }
-    },
-    series: [
-      {
-        name: "Completed Challange",
-        data: this.props.codewarsProfile.completedByCategory
-          ? this.props.codewarsProfile.completedByCategory
-              .completedChallangesByNumber
-          : []
-      }
-    ]
+    options: null,
+    series: null,
+    reRender: false
   };
+
+  componentDidMount() {
+    if (this.props.codewarsProfile.completedByCategory && !this.state.reRender)
+      this.setState({
+        reRender: true,
+        options: {
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              columnWidth: "10%"
+            }
+          },
+          dataLabels: {
+            enabled: true
+          },
+          xaxis: {
+            categories: this.props.codewarsProfile.completedByCategory
+              ? this.props.codewarsProfile.completedByCategory
+                  .completedChallangesByCatName
+              : []
+          },
+          title: {
+            text: "Completed Challanges By Category",
+            align: "center"
+          }
+        },
+        series: [
+          {
+            name: "Completed Challange",
+            data: this.props.codewarsProfile.completedByCategory
+              ? this.props.codewarsProfile.completedByCategory
+                  .completedChallangesByNumber
+              : []
+          }
+        ]
+      });
+  }
 
   render() {
     console.log(this.props.codewarsProfile);
